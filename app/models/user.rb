36c016d0dbc3fef087fc39938c55799e22e49165
Bradford Goodpasture <bgoodpasture@gmail.com>
2017-05-11 19:30:35 -0400

    rename table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_one :zodiac
+  has_many :journals
+  has_many :predicts, through: :journals
 end
